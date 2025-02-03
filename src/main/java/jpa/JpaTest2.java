4bc9f75d2d384e5dbf754860f46a5641fcec233a
Savi Oumar <oumar.savi@etudiant.univ-rennes.fr>
2025-02-03 12:19:37 +0100

    create jpatest2 for persistance in dbb

---
+++ src/main/java/jpa/JpaTest2.java
@@ -0,0 +1,62 @@
+package jpa;
+
+import java.util.List;
+
+import Models.Department;
+import Models.Employee;
+
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.EntityTransaction;
+import jakarta.persistence.Persistence;
+
+public class JpaTest2 {
+
+    private EntityManager manager;
+
+    public JpaTest2(EntityManager manager) {
+        this.manager = manager;
+    }
+    /**
+     * @param args
+     */
+    public static void main(String[] args) {
+        EntityManagerFactory factory =
+                Persistence.createEntityManagerFactory("dev");
+        EntityManager manager = factory.createEntityManager();
+        JpaTest2 test = new JpaTest2(manager);
+
+        EntityTransaction tx = manager.getTransaction();
+        tx.begin();
+        try {
+            test.createEmployees();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        tx.commit();
+
+        test.listEmployees();
+
+        manager.close();
+        System.out.println(".. done");
+    }
+
+    private void createEmployees() {
+        int numOfEmployees = manager.createQuery("Select a From Employee a", Employee.class).getResultList().size();
+        if (numOfEmployees == 0) {
+            Department department = new Department("java");
+            manager.persist(department);
+
+            manager.persist(new Employee("Jakab Gipsz",department));
+            manager.persist(new Employee("Captain Nemo",department));
+
+        }
+    }
+
+    private void listEmployees() {
+        List<Employee> resultList = manager.createQuery("Select a From Employee a", Employee.class).getResultList();
+        System.out.println("num of employess:" + resultList.size());
+        for (Employee next : resultList) {
+            System.out.println("next employee: " + next);
+        }
+    }}
