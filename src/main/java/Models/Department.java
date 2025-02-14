8f32bf8bee879f48d41f27fa1db13c8c62f052a2
Lagloire Choudenou <lchoudenou@univ-rennes1.fr>
2025-02-14 16:39:52 +0100

    testjpa modified

--- src/main/java/Models/Department.java
+++ src/main/java/Models/Department.java
@@ -17,7 +17,7 @@ public class Department {
     private String name;
 
     private List<Employee> employees = new ArrayList<Employee>();
-
+// CONTRUCTEUR
     public Department() {
         super();
     }
@@ -25,6 +25,7 @@ public class Department {
     public Department(String name) {
         this.name = name;
     }
+//    Un constructeur par défaut nécessaire pour JPA.
 
     @Id
     @GeneratedValue
@@ -38,11 +39,16 @@ public class Department {
 
     public String getName() {
         return name;
-    }
+    } //    Getter qui retourne le nom du département.
 
     public void setName(String name) {
         this.name = name;
-    }
+    }//    Setter qui modifie le nom du département.
+
+//    Gestion de la relation OneToMany avec Employee
+//    @OneToMany → Un département peut contenir plusieurs employés (relation 1-N)
+//    mappedBy = "department" → Indique que l’attribut department dans Employee est responsable de la relation.
+//    cascade = CascadeType.PERSIST → Si on ajoute un département, ses employés seront enregistrés automatiquement en base.
 
     @OneToMany(mappedBy = "department", cascade = CascadeType.PERSIST)
     public List<Employee> getEmployees() {
--- src/main/java/Models/Employee.java
+++ src/main/java/Models/Employee.java
@@ -1,26 +1,30 @@
 package Models;
-
+//Même package que Department, ce qui permet aux classes d’interagir facilement.
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
 import jakarta.persistence.ManyToOne;
 
+//@Entity → Indique que cette classe est une table en base de données
 @Entity
 public class Employee {
     private Long id;
 
     private String name;
 
+   // department stocke une référence vers le département auquel l'employé appartient.
     private Department department;
 
     public Employee() {
     }
 
+   // Constructeur par défaut requis par JPA.
     public Employee(String name, Department department) {
         this.name = name;
         this.department = department;
     }
 
+    //Constructeur pour créer un employé avec un nom et un département.
     public Employee(String name) {
         this.name = name;
     }
@@ -43,16 +47,21 @@ public class Employee {
         this.name = name;
     }
 
+    //Gestion de la relation ManyToOne avec Department
+    //@ManyToOne → Plusieurs employés peuvent appartenir au même département (relation N-1).
+
     @ManyToOne
     public Department getDepartment() {
         return department;
     }
 
+    //Setter pour définir le département de l’employé.
     public void setDepartment(Department department) {
         this.department = department;
     }
 
     @Override
+   // Méthode toString() Cette méthode permet d’afficher un employé sous forme lisible.
     public String toString() {
         return "Employee [id=" + id + ", name=" + name + ", department="
                 + department.getName() + "]";
